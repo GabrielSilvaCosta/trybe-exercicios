2ac445e218c75ba6b17394d5e7cc6d4439740ae3
GabrielSilvaCosta <bob@>
2023-01-23 15:37:01 -0300

    atividade if/else

---
+++ Fundamentos/bloco-02-introducao-html-css/dia1/scripts2.js
@@ -0,0 +1,13 @@
+const nota = 70;
+
+if (nota >= 80) {
+    console.log('parabens, voce faz parte do grupo de pessoas aprovadas.');
+
+} else if (nota < 80 && nota >= 60) {
+    console.log('voce esta na nossa lista de espera');
+
+
+} else {
+    console.log('infelizmente, voce reprovou');
+
+}
